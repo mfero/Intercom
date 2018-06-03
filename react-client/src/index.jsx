e3106db5eeadfcb8218645a9196b16d58653b120
Michael Fero <mike.fero@teselagen.com>
2018-06-02 23:06:54 -0700

    Update index.jsx

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -1,2 +1,5 @@
 <script type="text/javascript" src="bundle.js"></script>
-
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+ReactDOM.render(<App />, document.getElementById('app'));
